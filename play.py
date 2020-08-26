e8c73e080028c5c9083ade9b5fbd318ea831b7fd
Sangmin Lee <sle175@kolmogorov.mng0ilyuxoluhgy1dnpxbaxgzg.ax.internal.cloudapp.net>
2020-08-26 11:47:33 +0000

    experiment data

--- play.py
+++ play.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 from instance_test import ggg, kdata
 from DQN import Env, DQN_Agent, ReplayMemory, train, test
 
-MAX_EPISODES = 3
+NUM_EXP = 30
+MAX_EPISODES = 1000
 PUNISHMENT = 0
 ARRIVAL_BONUS = 0
 
@@ -33,8 +34,7 @@ destination = np.array([ggg.vs.select(name = i).indices[0] for i in kdata[1,:]])
 env = Env(ggg, origin, destination, kdata[2,:], ARRIVAL_BONUS)
 
 #%%
-NUM_EXP = 2
-setup_dict = {'max_episodes':MAX_EPISODES, 'punishment':PUNISHMENT, 'arrival_bonus':ARRIVAL_BONUS, 'alpha':ALPHA, 'gamma':GAMMA, 'eps_start':EPS_START, 'eps_end':EPS_END, 'eps_decay':EPS_DECAY, 'batch_size':BATCH_SIZE, 'target_update':TARGET_UPDATE, 'memory_size':MEMORY_SIZE, 'hidden_dim1':HIDDEN_DIM1, 'hidden_dim2':HIDDEN_DIM2}
+setup_dict = {'num_exp':NUM_EXP, 'max_episodes':MAX_EPISODES, 'punishment':PUNISHMENT, 'arrival_bonus':ARRIVAL_BONUS, 'alpha':ALPHA, 'gamma':GAMMA, 'eps_start':EPS_START, 'eps_end':EPS_END, 'eps_decay':EPS_DECAY, 'batch_size':BATCH_SIZE, 'target_update':TARGET_UPDATE, 'memory_size':MEMORY_SIZE, 'hidden_dim1':HIDDEN_DIM1, 'hidden_dim2':HIDDEN_DIM2}
 
 
 #%%
@@ -75,40 +75,40 @@ with open('/home/sle175/rlcombopt/data/%s__%s.p' % (file_name1, file_name2), 'wb
 
 
 #%% NEED FOR PLOTTING
-from scipy.stats import t
-xx = np.arange(1, MAX_EPISODES+1)
-avg = [np.average([EXP_DATA[i][0][j].sum() for i in range(NUM_EXP)]) for j in range(MAX_EPISODES)]
-se = np.array([np.std([EXP_DATA[i][0][j].sum() for i in range(NUM_EXP)], ddof = 1) for j in range(MAX_EPISODES)])/np.sqrt(NUM_EXP)
-tvalue = t.ppf(1-0.025, NUM_EXP-1)
-best = [EXP_DATA[i][5] for i in range(NUM_EXP)]
+#from scipy.stats import t
+#xx = np.arange(1, MAX_EPISODES+1)
+#avg = [np.average([EXP_DATA[i][0][j].sum() for i in range(NUM_EXP)]) for j in range(MAX_EPISODES)]
+#se = np.array([np.std([EXP_DATA[i][0][j].sum() for i in range(NUM_EXP)], ddof = 1) for j in range(MAX_EPISODES)])/np.sqrt(NUM_EXP)
+#tvalue = t.ppf(1-0.025, NUM_EXP-1)
+#best = [EXP_DATA[i][5] for i in range(NUM_EXP)]
 
 #%%
-GUROBI = -15459
+#GUROBI = -15459
 
 #%% PLOT 1 
-plt.figure(figsize = (10,6))
-plt.fill_between(xx, avg-tvalue*se,
-                 avg+tvalue*se,
-                 alpha=.3, color = '#69b7d6')
-plt.plot(xx, avg, color = '#69b7d6', label = 'Average Learning Curve of RL')
-plt.axhline(y = GUROBI, color = 'r', label = 'Best Solution found by Gurobi')
-plt.axhline(y = np.max(best), color = 'b', label = 'Best Solution found by RL')
-plt.xlabel('\n Episodes', fontsize = 20)
-plt.ylabel('Total Reward Sum of all agents', fontsize = 20)
-plt.tick_params(axis = 'both', size = 10, labelsize = 12)
-plt.legend(frameon=False, bbox_to_anchor=(0.99, 0.03), loc= "lower right", fontsize = 12)
+#plt.figure(figsize = (10,6))
+#plt.fill_between(xx, avg-tvalue*se,
+#                 avg+tvalue*se,
+#                 alpha=.3, color = '#69b7d6')
+#plt.plot(xx, avg, color = '#69b7d6', label = 'Average Learning Curve of RL')
+#plt.axhline(y = GUROBI, color = 'r', label = 'Best Solution found by Gurobi')
+#plt.axhline(y = np.max(best), color = 'b', label = 'Best Solution found by RL')
+#plt.xlabel('\n Episodes', fontsize = 20)
+#plt.ylabel('Total Reward Sum of all agents', fontsize = 20)
+#plt.tick_params(axis = 'both', size = 10, labelsize = 12)
+#plt.legend(frameon=False, bbox_to_anchor=(0.99, 0.03), loc= "lower right", fontsize = 12)
 
 #%% PLOT 2
-plt.figure(figsize = (10,6))
-plt.axhline(y = GUROBI, color = 'r', label = 'Best Solution found by Gurobi')
-plt.axhline(y = np.average(best), alpha = 0.8, color = '#69b7d6', label = 'Average of Best Solutions found by RL')
-plt.scatter(np.arange(1,NUM_EXP+1), best, color = 'b',
-            label = 'Best Solution found by RL in each experiment')
-plt.xlabel('\n Experiment', fontsize = 20)
-plt.ylabel('Total Reward Sum of all agents', fontsize = 20)
-plt.tick_params(axis = 'both', size = 10, labelsize = 12)
-plt.ylim((min(best)-1200, GUROBI+200))          
-plt.legend(frameon=False, loc= "lower left", fontsize = 12)
+#plt.figure(figsize = (10,6))
+#plt.axhline(y = GUROBI, color = 'r', label = 'Best Solution found by Gurobi')
+#plt.axhline(y = np.average(best), alpha = 0.8, color = '#69b7d6', label = 'Average of Best Solutions found by RL')
+#plt.scatter(np.arange(1,NUM_EXP+1), best, color = 'b',
+#            label = 'Best Solution found by RL in each experiment')
+#plt.xlabel('\n Experiment', fontsize = 20)
+#plt.ylabel('Total Reward Sum of all agents', fontsize = 20)
+#plt.tick_params(axis = 'both', size = 10, labelsize = 12)
+#plt.ylim((min(best)-1200, GUROBI+200))          
+#plt.legend(frameon=False, loc= "lower left", fontsize = 12)
 
 
 
--- timetest.py
+++ timetest.py
@@ -2,6 +2,7 @@
 #%%
 import time
 import torch
+import numpy as np
 
 device = torch.device("cuda")
 
@@ -14,10 +15,24 @@ end = time.time()
 
 print("cpu time: " + str(end-start))
 
-start_g = time.time()
 mat1_g = mat1.to(device)
 mat2_g = mat2.to(device)
+start_g = time.time()
 rst = torch.matmul(mat1_g, mat2_g)
 end_g = time.time()
 
 print("gpu time:" + str(end_g-start_g))
+
+tmp = np.random.rand(100)
+st = time.time()
+torch.from_numpy(tmp).to(device)
+ed = time.time()
+print("to_device: " + str(ed-st))
+
+st = time.time()
+torch.from_numpy(tmp).cuda()
+ed = time.time()
+print("cuda: " + str(ed-st))
+
+
+
